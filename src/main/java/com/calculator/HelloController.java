f820ac27d8e41c877f0421fb0eb396bfe9395444
mojisilva <mojiasilva@gmail.com>
2022-03-05 20:29:38 -0300

    Criação do layout da calculadora

--- src/main/java/com/calculator/HelloController.java
+++ src/main/java/com/calculator/HelloController.java
@@ -1,17 +1,74 @@
 package com.calculator;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class HelloController {
     @FXML
     private Label outputText;
+
+    @FXML
+    private TextField inputText;
+
     @FXML
     private AnchorPane anchorPane;
 
     @FXML
+    private Button one;
+
+    @FXML
+    private Button two;
+
+    @FXML
+    private Button three;
+
+    @FXML
+    private Button four;
+
+    @FXML
+    private Button five;
+
+    @FXML
+    private Button six;
+
+    @FXML
+    private Button seven;
+
+    @FXML
+    private Button eight;
+
+    @FXML
+    private Button nine;
+
+    @FXML
+    private Button zero;
+
+    @FXML
+    private Button sum;
+
+    @FXML
+    private Button sub;
+
+    @FXML
+    private Button div;
+
+    @FXML
+    private Button multi;
+
+    @FXML
+    private Button dot;
+
+    @FXML
+    private Button equal;
+
+    @FXML
+    private Button clear;
+
+    @FXML
     protected void onHelloButtonClick() {
-        outputText.setText("Welcome to JavaFX Application!");
+        outputText.setText(inputText.getText());
     }
 }

389f4d3cbe75ab6b596184b7ef041a1ac7f85d1e
mojisilva <mojiasilva@gmail.com>
2022-03-08 22:46:35 -0300

    alteração nas nomenclaturas do arquivo fxml e das classes controller e application e correções nos comportamentos dos notões

--- src/main/java/com/calculator/CalculatorApplication.java
+++ src/main/java/com/calculator/CalculatorApplication.java
@@ -3,16 +3,19 @@ package com.calculator;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
-public class HelloApplication extends Application {
+public class CalculatorApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(CalculatorApplication.class.getResource("calculator-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 478);
         stage.setTitle("Calculator");
+        stage.getIcons().add(new Image(Objects.requireNonNull(CalculatorApplication.class.getResourceAsStream("icon.png"))));
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/calculator/CalculatorController.java
@@ -0,0 +1,131 @@
+package com.calculator;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+
+public class CalculatorController {
+    double a = 0;
+    double b = 0;
+    double result = 0;
+    String operation;
+
+    @FXML
+    private TextField inputTF;
+
+    @FXML
+    protected void clearButtonClick() {
+        inputTF.clear();
+        a = 0;
+        b = 0;
+        result = 0;
+        operation = "";
+    }
+
+    @FXML
+    public void zeroButtonClick() {
+        inputTF.setText(inputTF.getText() + "0");
+    }
+
+    @FXML
+    public void oneButtonClick() {
+        inputTF.setText(inputTF.getText() + "1");
+    }
+
+    @FXML
+    public void twoButtonClick() {
+        inputTF.setText(inputTF.getText() + "2");
+    }
+
+    @FXML
+    public void threeButtonClick() {
+        inputTF.setText(inputTF.getText() + "3");
+    }
+
+    @FXML
+    public void fourButtonClick() {
+        inputTF.setText(inputTF.getText() + "4");
+    }
+
+    @FXML
+    public void fiveButtonClick() {
+        inputTF.setText(inputTF.getText() + "5");
+    }
+
+    @FXML
+    public void sixButtonClick() {
+        inputTF.setText(inputTF.getText() + "6");
+    }
+
+    @FXML
+    public void sevenButtonClick() {
+        inputTF.setText(inputTF.getText() + "7");
+    }
+
+    @FXML
+    public void eightButtonClick() {
+        inputTF.setText(inputTF.getText() + "8");
+    }
+
+    @FXML
+    public void nineButtonClick() {
+        inputTF.setText(inputTF.getText() + "9");
+    }
+
+    @FXML
+    public void dotButtonClick() {
+        inputTF.setText(inputTF.getText() + ".");
+    }
+
+    @FXML
+    public void multiButton() {
+        a = Double.parseDouble(inputTF.getText());
+        inputTF.setText("");
+        operation = "*";
+    }
+
+    @FXML
+    public void divButton() {
+        a = Double.parseDouble(inputTF.getText());
+        inputTF.setText("");
+        operation = "/";
+    }
+
+    @FXML
+    public void sumButton() {
+        a = Double.parseDouble(inputTF.getText());
+        inputTF.setText("");
+        operation = "+";
+    }
+
+    @FXML
+    public void subButton() {
+        a = Double.parseDouble(inputTF.getText());
+        inputTF.setText("");
+        operation = "-";
+    }
+
+
+    @FXML
+    public void equalButton() {
+        b = Double.parseDouble(inputTF.getText());
+        switch (operation) {
+            case "*" -> {
+                result = a * b;
+                inputTF.setText(String.valueOf(result));
+            }
+            case "/" -> {
+                result = a / b;
+                inputTF.setText(String.valueOf(result));
+            }
+            case "+" -> {
+                result = a + b;
+                inputTF.setText(String.valueOf(result));
+            }
+            case "-" -> {
+                result = a - b;
+                inputTF.setText(String.valueOf(result));
+            }
+        }
+    }
+}
